a73f35569da88ef85a28cdc241b6b7b760013ca2
Mark Huntjens <mark.huntjens@isatis-business-solutions.nl>
2018-05-31 13:22:08 +0200

    finished

---
+++ Chapter 8 - ListOfDucks/Duck.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter_8___ListOfDucks
+{
+    class Duck//: IComparable<Duck>
+    {
+        public int Size;
+        public KindOfDuck Kind;
+
+        public void Quack()
+        {
+
+        }
+        public void Swim()
+        {
+
+        }
+        public void Eat()
+        {
+
+        }
+        public void Walk()
+        {
+
+        }
+
+        public int CompareTo(Duck duckToCompare)
+        {
+            if (this.Size > duckToCompare.Size)
+                return 1;
+            else if (this.Size < duckToCompare.Size)
+                return -1;
+            else
+                return 0;
+        }
+    }
+
+    enum KindOfDuck
+    {
+        Mallard,
+        Muscovy,
+        Decoy,
+    }
+}
--- Chapter 8 - ListOfDucks/Program.cs
+++ Chapter 8 - ListOfDucks/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,34 @@ namespace Chapter_8___ListOfDucks
     {
         static void Main(string[] args)
         {
+            List<Duck> ducks = new List<Duck>()
+            {
+                new Duck() {Kind = KindOfDuck.Mallard, Size = 17},
+                new Duck() {Kind = KindOfDuck.Muscovy, Size = 18},
+                new Duck() {Kind = KindOfDuck.Decoy, Size = 14},
+                new Duck() {Kind = KindOfDuck.Muscovy, Size = 11},
+                new Duck() {Kind = KindOfDuck.Mallard, Size = 14},
+                new Duck() {Kind = KindOfDuck.Decoy, Size = 13},
+            };
+            //ducks.Sort(); //works when class Duck implements IComparable<Duck> which has CompareTo() method which makes Sort() work
+            //DuckComparerBySize sizeComparer = new DuckComparerBySize();
+            //ducks.Sort(sizeComparer);
+
+            DuckComparerByKind kindComparer = new DuckComparerByKind();
+            ducks.Sort(kindComparer);
+
+            PrintDucks(ducks);
+            Console.ReadKey();
+        }
+
+        public static void PrintDucks(List<Duck> ducks)
+        {
+            foreach (Duck duck in ducks)
+            {
+                //Console.WriteLine($"{duck.Size}-inch {duck.Kind}");
+                Console.WriteLine(duck.Size.ToString() + "-inch " + duck.Kind.ToString());
+            }
+            Console.WriteLine("End of ducks");
         }
     }
 }
